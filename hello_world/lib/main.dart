ac2012403caad54e2cb85ff320d4b7bff7dde1b0
AyshaAraien <aaraien@ualberta.ca>
2020-05-06 18:34:03 -0600

    feat: adding scroll bars and favorite

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.pink[300]),
-      home: Scaffold(
-        // Note that you can't just set a string, it needs to be a widget. So that's why we wrap it in a Text widget.
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("WordPair Generator")),
-        body: Center(child: Text(wordPair.asPascalCase)),
-        )
+      home: RandomWords()
       );
   }
 }
